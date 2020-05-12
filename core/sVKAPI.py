f58efa7c06d790c11f3d7fec1cbee925eabee26b
SANYAINC <sanyaincorp@gmail.com>
2020-05-12 20:54:29 +0300

    User agent added

--- core/sVKAPI.py
+++ core/sVKAPI.py
@@ -17,3 +17,6 @@ class VkAPI:
         def inputCaptcha() -> str:
             return input("Captcha: ")
 
+        headers: dict = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                                       " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
+
