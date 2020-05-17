133f23579642b9b54be390b456bff303214d66bb
alasaijir <sanyaincorp@gmail.com>
2020-05-17 18:25:31 +0300

    PyPi setup modified

--- sVKAPI/setup.py
+++ sVKAPI/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='sVKAPI',
-      version='1.0.0.1',
-      description='Library for using VK API - ',
+      version='1.0.0.1.2',
+      description='Library for using VK API',
       url='https://github.com/alasaijir/sVKAPI',
       packages=['sVKAPI'],
       author='alasaijir',
